3b64b771a5fd2e16f8027d336f6797f24ef17c33
nVr <labaouici02@gmail.com>
2020-08-29 21:45:43 +0200

    Commit de révision

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/vue1', function () {
+    return view('vue1');
+});
+
+// permet, avec article.blade.php de faire une page qui selon l'URL diras (je suis l'article x)
+
+Route::get('article/{n}', function($n) {
+    return view('article')->with('numero', $n);
+})->where('n', '[0-9]+');
+
+Route::get('facture/{n}', function ($n) {
+    return view('facture')->withNumero($n);
+})->where('n', '[0-9]+');
+
+Route::get('{n}', function($n) {
+    return response('Je suis la page ' . $n . ' !', 200);
+})->where('n', '[1-3]');
